d5a6fc4b4934ad438d4c1640a7ba31d1686a090b
Jadranka Stupar <jaca.stupar@gmail.com>
2022-12-27 01:37:01 +0100

    Commented REST in main app.

--- src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -1,21 +1,28 @@
 package com.javatechie.jenkin.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
+@Slf4j
 @SpringBootApplication
-@RestController
+//@RestController
 public class SpringJenkinsApplication {
 
 
-	@GetMapping("/ping")
-	public String message() {
-		return "Wao!!! Application successfully deployed in SAP Cloud.";
-	}
+//	@GetMapping("/ping")
+//	public String message() {
+//		return "Wao!!! Application successfully deployed in SAP Cloud.";
+//	}
 
+	@PostConstruct
+	public void intt() {
+		log.info("*** Application started...");
+	}
 	public static void main(String[] args) {
+		log.info("*** Application executed...");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
--- src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -2,6 +2,7 @@ package com.javatechie.jenkin.api;
 
 import static org.junit.Assert.assertEquals;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -9,15 +10,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringJenkinsApplicationTests {
 
-	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
-
 	@Test
 	public void contextLoads() {
-		logger.info("Test case executing...");
+		log.info("Test case executing...");
 		assertEquals(true, true);
 	}
 
