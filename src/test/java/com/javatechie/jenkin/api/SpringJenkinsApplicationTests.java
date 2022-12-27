313ad332348fc27a10eac483512d20ec70e73dbc
Jadranka Stupar <jaca.stupar@gmail.com>
2022-12-27 02:54:57 +0100

    Updated test part.

--- src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -1,17 +1,12 @@
 package com.javatechie.jenkin.api;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @Slf4j
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringJenkinsApplicationTests {
 
