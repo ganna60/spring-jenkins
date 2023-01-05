b77bfff79dababac2e9a2d93243e598eb34c2081
Jadranka Stupar <jaca.stupar@gmail.com>
2023-01-05 15:51:51 +0100

    Update 14 JAN 2023

--- src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -3,19 +3,21 @@ package com.javatechie.jenkin.api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 
 @Slf4j
 @SpringBootApplication
-//@RestController
+@RestController
 public class SpringJenkinsApplication {
 
 
-//	@GetMapping("/ping")
-//	public String message() {
-//		return "Wao!!! Application successfully deployed in SAP Cloud.";
-//	}
+	@GetMapping("/ping")
+	public String message() {
+		return "Wao!!! Application successfully deployed in SAP Cloud.";
+	}
 
 	@PostConstruct
 	public void intt() {
