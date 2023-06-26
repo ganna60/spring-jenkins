39ee503abce126904d7a62f48f8ef72e2e74be81
Jadranka S <jaca.stupar@gmail.com>
2023-06-26 14:06:14 +0200

    Remove void from test app - error.

--- src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -12,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class SpringJenkinsApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public contextLoads() {
 		log.info("Test case executing...");
 		log.info("Test case executing...2");
 		assertEquals(true, true);
